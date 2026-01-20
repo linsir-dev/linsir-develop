bfdf526831620e1199c75aa38ab2685e403bae73
Linsir <32347383+linsir-dev@users.noreply.github.com>
2026-01-21 05:07:45 +0800

    Add OOP demo: Dog, Cat, Walkable, PetOwner, Main

---
+++ linsir-abc/linsir-abc-pdai/src/main/java/com/linsir/abc/pdai/oopdemo/Dog.java
@@ -0,0 +1,29 @@
+package com.linsir.abc.pdai.oopdemo;
+
+public class Dog extends Animal implements Walkable {
+    private String breed;
+
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(getName() + " says: Woof!");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println(getName() + " the " + breed + " is walking.");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " [Dog, breed=" + breed + "]";
+    }
+}
