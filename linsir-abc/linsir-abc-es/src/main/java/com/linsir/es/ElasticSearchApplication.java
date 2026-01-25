ced517ead1fc666e38809e90cdfbd3338884970a
linsir <student_yxl@126.com>
2026-01-25 20:29:15 +0800

    构建Spring Boot框架，添加ElasticSearch相关配置

---
+++ linsir-abc/linsir-abc-es/src/main/java/com/linsir/es/ElasticSearchApplication.java
@@ -0,0 +1,14 @@
+package com.linsir.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ElasticSearch应用启动类
+ */
+@SpringBootApplication
+public class ElasticSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticSearchApplication.class, args);
+    }
+}
